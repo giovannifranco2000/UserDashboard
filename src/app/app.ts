c1501602455aa198303454d2f33e0403fd056859
Giovanni Franco <giovannifranco2000@gmail.com>
2025-11-09 18:33:21 +0100

    add app component structure and logic

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, inject, Signal, signal } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer';
+import { HeaderComponent } from './components/header/header';
+import { HeaderService } from './components/header/header.service';
+import { ContainerComponent } from './containers/container/container';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ContainerComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('user-dashboard-app');
+
+  private titleService = inject(Title);
+  private headerService = inject(HeaderService);
+
+  protected readonly appTitle: Signal<string> = signal("Dashboard");
+
+  constructor() {
+    effect(() => {
+      this.titleService.setTitle(`${this.appTitle()} | ${this.headerService.getTitle()}`);
+    });
+  }
+
 }
